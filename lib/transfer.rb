2219818f10c1a7ca7d73005be1a0e274914dd2e0
Eyal Toledano <eyaltoledano@me.com>
2019-03-22 21:02:02 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -16,7 +16,7 @@ class Transfer
 
   def execute_transaction
     binding.pry
-    if amount > sender.balance
+    if self.amount > self.sender.balance
       "Transaction rejected. Please check your account balance."
       self.status = "rejected"
     end
